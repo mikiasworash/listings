ccfcf3334d607d2aa53baf8643d9b40c9f346a78
Mikias Worash <mikiasworash@gmail.com>
2024-02-24 14:13:19 +0300

    added listings

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Listings from "../components/ProductLIst";
+
+const Home = () => {
+  return (
+    <div className="h-screen w-full">
+      <Listings />
+    </div>
+  );
+};
+export default Home;
